6e327b6a039af39cfcb5f53ac4f159d059e19350
Harold Espinoza <haroldesptru@gmail.com>
2019-07-20 23:33:18 -0600

    Añadidas directivas para el header y así evitar el almacenamiento en cache de los archivos.

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ require_once 'templates/templates.php';
 
 $router = new Router();
 
+// header directives for evit file caching
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Connection: close");
+
 
 $router->error('404', function(){
     return "<h1>Oh, Oh. No logramos encontrar lo que buscas.</h1>";
