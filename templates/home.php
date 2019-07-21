6ec9239113c2b5b90e7eb9adec4198fd32db7664
Harold Espinoza <haroldesptru@gmail.com>
2019-07-20 23:35:31 -0600

    Añadidos estilos base de reseteo. Añadidas fuentes a utilizar. Se cambió la estructura del cover, ahora se utilizará una imagen en lugar del video de cover.

--- templates/home.php
+++ templates/home.php
@@ -8,10 +8,25 @@ function HomePage(){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Come rico en el mejor restaurante de la ciudad, servicio de primera.">
+    <meta name="author" content="Harold Espinoza | haroldesptru@gmail.com">
     <title>Restaurant Center</title>
+    <link rel="stylesheet" href="public/css/Montserrat.css" >
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
+
+    <!--[if lt IE 9]>
+        <p class='browserupgrade'>
+            Estás usando un navegador
+            <strong>desactualizado.</strong>
+            Por favor actualiza tu navegador para una mejor experiencia.
+        </p>
+    <![endif]-->
+    
+    <img src="./public/img/cover.jpg" alt="">
+    
+    <h2>Test font family</h2>
 
 
 <script src="public/js/main.js"></script>
